9f3f3dfd39f14e800086ea56c2799f7c7f6088e7
AndyLiang1 <andyliang1000@gmail.com>
2024-08-07 20:29:29 -0700

    Added basic CSS for page layouts.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,20 +2,8 @@ import React from 'react';
 import { HomePage } from './pages/HomePage';
 
 function App() {
-  const handleButtonClick = async () => {
-    try {
-      const response = await fetch('http://localhost:3002/');
-      const data = await response.json();
-      console.log(data); // Handle the response data as needed
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
-
   return (
-    <div className="App">
-      I love you
-      <button onClick={handleButtonClick}>Hit Backend</button>
+    <div className="App h-screen v-screen">
       <HomePage />
     </div>
   );
---
+++ client/src/components/SideBar.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface ISideBarProps {
+}
+
+export function SideBar (props: ISideBarProps) {
+  return (
+    <div className = "w-[15%] h-full bg-blue-50">
+      Sidebar
+    </div>
+  );
+}
