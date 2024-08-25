3b5bc81a732768bb965d5bdc16b9e13e27a2e5b0
aryaphan <aryaphan31@gmail.com>
2024-08-24 18:30:22 -0700

    use alembic migration

--- db/yoyo_migrations/tables/0000.create-table-user.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS our_story.user (
-    user_id TEXT PRIMARY KEY DEFAULT concat('user-', gen_random_uuid()),
-    email TEXT,
-    password TEXT,
-    created_at_utc TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),
-    updated_at_utc TIMESTAMP WITHOUT TIME ZONE
-);
