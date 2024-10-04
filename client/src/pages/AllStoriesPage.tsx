4fabf11b1cc57cf25dbfe6114c0c0cbefe367e63
AndyLiang1 <andyliang1000@gmail.com>
2024-10-03 17:29:03 -0700

    Added all stories page.

---
+++ client/src/pages/AllStoriesPage.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { getAllDocuments } from '../apis/documentApi';
+import { CreateDocumentForm } from '../components/CreateDocumentForm';
+import { NavBar } from '../components/Navbar';
+import { DocumentData } from '../types/DocumentTypes';
+import { User } from '../types/UserTypes';
+
+export interface IAllStoriesPageProps {}
+
+export function AllStoriesPage(props: IAllStoriesPageProps) {
+    const [user, setUser] = useState<User>(useLocation().state);
+    const [documents, setDocuments] = useState<DocumentData[]>([]);
+    const [refetchTrigger, setRefetchTrigger] = useState<Object>({});
+    const [showForm, setShowForm] = useState<boolean>(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            if (user.collabToken) {
+                const documents = await getAllDocuments(user.userId, user.collabToken, null, null);
+                setDocuments(documents);
+            }
+        };
+        fetchData();
+    }, [user, refetchTrigger]);
+
+    return (
+        <div className="v-screen relative h-screen flex-wrap items-center">
+            {showForm && user && (
+                <CreateDocumentForm
+                    user={user}
+                    setShowForm={setShowForm}
+                    setRefetchTrigger={setRefetchTrigger}
+                />
+            )}
+            <NavBar setShowForm={setShowForm} />
+            {documents.length ? <div>Docs</div> : <div>Loading</div>}
+        </div>
+    );
+}
