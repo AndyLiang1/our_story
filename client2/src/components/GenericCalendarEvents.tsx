ad8f2cae5b34f8e619ad0808bfe5f61dcf87a0ce
AndyLiang1 <andyliang1000@gmail.com>
2025-03-20 20:25:27 -0700

    Calendar now shows the month of the date of the doc clicked and the event date now has different CSS.

--- client2/src/components/GenericCalendarEvents.tsx
+++ client2/src/components/GenericCalendarEvents.tsx
@@ -4,24 +4,37 @@ import { Tooltip } from './Tooltip';
 export interface IGenericCalendarEventsProps {
     events: EventMetaData[];
     handleClick: (id: string) => void;
+    currEventId?: string;
 }
 
-export function GenericCalendarEvents({ events, handleClick }: IGenericCalendarEventsProps) {
+export function GenericCalendarEvents({
+    events,
+    handleClick,
+    currEventId
+}: IGenericCalendarEventsProps) {
     return (
         <div className="flex h-[45%] w-full items-center justify-evenly">
             {events.map((event, index) => {
                 return (
                     <div
-                        key = {index}
+                        key={index}
                         className="flex h-2 w-2 items-center justify-center"
                         onClick={() => handleClick(event.id)}
                     >
                         <Tooltip tooltipContent={event.name}>
-                            <img
-                                src="/Light_Blue_Circle.png"
-                                className="event-img h-1 max-h-2 w-1 max-w-2"
-                                alt=""
-                            />
+                            {event.id === currEventId ? (
+                                <img
+                                    src="/Light_Red_Circle.png"
+                                    className="event-img h-1 max-h-2 w-1 max-w-2"
+                                    alt=""
+                                />
+                            ) : (
+                                <img
+                                    src="/Light_Blue_Circle.png"
+                                    className="event-img h-1 max-h-2 w-1 max-w-2"
+                                    alt=""
+                                />
+                            )}
                         </Tooltip>
                     </div>
                 );
