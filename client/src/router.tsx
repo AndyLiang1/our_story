68fb48adaedaedd0e5e8fe66d3f42ba0572c4477
AndyLiang1 <andyliang1000@gmail.com>
2025-04-21 12:27:06 -0700

    Disabled signup page for now.

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -3,7 +3,6 @@ import { AllStoriesPage } from './pages/AllStoriesPage';
 import { ConfirmUserPage } from './pages/ConfirmUserPage';
 import { HomePage } from './pages/HomePage';
 import { LoginPage } from './pages/LoginPage';
-import { SignUpPage } from './pages/SignUpPage';
 
 export const router = createBrowserRouter([
     {
@@ -18,10 +17,10 @@ export const router = createBrowserRouter([
         path: '/stories',
         element: <AllStoriesPage />
     },
-    {
-        path: '/signup',
-        element: <SignUpPage />
-    },
+    // {
+    //     path: '/signup',
+    //     element: <SignUpPage />
+    // },
     {
         path: '/confirm',
         element: <ConfirmUserPage />
