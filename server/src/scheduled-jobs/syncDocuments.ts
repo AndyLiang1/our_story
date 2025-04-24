2a5c2e4f05e4bd0770627a8c4c3522ab78b6b59a
AndyLiang1 <andyliang1000@gmail.com>
2025-04-24 08:01:37 -0700

    Minor refactor to sync.

--- server/src/scheduled-jobs/syncDocuments.ts
+++ server/src/scheduled-jobs/syncDocuments.ts
@@ -5,6 +5,7 @@ export const syncDocuments = () => {
     cron.schedule(
         '0 0 * * *',
         async () => {
+            console.log("Begin sync...")
             const numberOfUpdatedDocs = await services.documentService.syncDocuments();
             console.log(`Synced ${numberOfUpdatedDocs} documents`);
         },
