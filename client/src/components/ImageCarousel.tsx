9d1ebb30c3a78ff872629772822a472375a43736
AndyLiang1 <andyliang1000@gmail.com>
2025-04-28 18:54:16 -0700

    Prevent spam clicking the delete image button so we do not send multiple api requests at the same time

--- client/src/components/ImageCarousel.tsx
+++ client/src/components/ImageCarousel.tsx
@@ -4,8 +4,8 @@ import { IoMdCloudUpload } from 'react-icons/io';
 import { getDocument } from '../apis/documentApi';
 import { deleteDocumentImages, getGeneratedDownloadImageSignedUrls } from '../apis/imageApi';
 import { useUserContext } from '../context/userContext';
-import { UploadImageModalInfo } from '../types/ModalInfoTypes';
 import { DocumentData } from '../types/DocumentTypes';
+import { UploadImageModalInfo } from '../types/ModalInfoTypes';
 
 export interface IImageCarouselProps {
     document: DocumentData;
@@ -25,17 +25,19 @@ export function ImageCarousel({
 }: IImageCarouselProps) {
     const user = useUserContext();
     const { collabToken } = user;
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const [signedImageUrlsWithGuidNames, setSignedImageUrlsWithGuidNames] = useState<
-        { signedImageUrl: string; imageNameWithGuid: string }[]
-    >([]);
+    const [signedImageUrlsWGuidNamesWDisplayValues, setSignedImageUrlsWGuidNamesWDisplayValues] =
+        useState<{ signedImageUrl: string; imageNameWithGuid: string; onDisplay: boolean }[]>([]);
     const DEFAULT_IMG_URL = '/autumn-landscape-building-city-blue-600nw-2174533935.png';
-
+    const [showDeleteConfirmationButton, setShowDeleteConfirmationButton] = useState(false);
     useEffect(() => {
-        getSignedImageUrls(null, document.documentId);
+        getAndSetSignedImageUrls(null, document.documentId, 0);
     }, []);
 
-    const getSignedImageUrls = async (imagesNameWGuid: string[] | null, documentId: string) => {
+    const getAndSetSignedImageUrls = async (
+        imagesNameWGuid: string[] | null,
+        documentId: string,
+        indexToDisplay: number
+    ) => {
         const listOfImageNamesWithGuid = imagesNameWGuid ? imagesNameWGuid : document.images;
         const { signedDownloadUrls } = await getGeneratedDownloadImageSignedUrls(
             collabToken,
@@ -45,9 +47,10 @@ export function ImageCarousel({
 
         const result = listOfImageNamesWithGuid.map((imageNameWithGuid: string, index: number) => ({
             signedImageUrl: signedDownloadUrls[index],
-            imageNameWithGuid
+            imageNameWithGuid,
+            onDisplay: index === indexToDisplay
         }));
-        setSignedImageUrlsWithGuidNames(result);
+        setSignedImageUrlsWGuidNamesWDisplayValues(result);
     };
 
     const resetUploadImageModalStateToInitial = () => {
@@ -61,44 +64,106 @@ export function ImageCarousel({
                 showUploadModalInfo.documentId === document.documentId &&
                 showUploadModalInfo.refetch
             ) {
-                const indexToBe = signedImageUrlsWithGuidNames.length;
+                const indexToBe = signedImageUrlsWGuidNamesWDisplayValues.length;
                 const doc = await getDocument(document.documentId, collabToken);
-                await getSignedImageUrls(doc.images, document.documentId);
+                getAndSetSignedImageUrls(doc.images, document.documentId, indexToBe);
                 resetUploadImageModalStateToInitial();
-                setCurrentIndex(indexToBe);
             }
         };
         fetchDocumentAndPopulateImagesForCurrentCarousel();
     }, [showUploadModalInfo]);
 
     const changeIndex = (direction: DIRECTION) => {
+        const signedImageUrlsWithGuidNamesWithUpdatedDisplayValues = [
+            ...signedImageUrlsWGuidNamesWDisplayValues
+        ];
+        const totalLength = signedImageUrlsWithGuidNamesWithUpdatedDisplayValues.length;
         if (direction === DIRECTION.LEFT) {
-            setCurrentIndex(
-                currentIndex !== 0 ? currentIndex - 1 : signedImageUrlsWithGuidNames.length - 1
-            );
+            for (const [
+                index,
+                imageInfo
+            ] of signedImageUrlsWithGuidNamesWithUpdatedDisplayValues.entries()) {
+                if (imageInfo.onDisplay) {
+                    signedImageUrlsWithGuidNamesWithUpdatedDisplayValues[index].onDisplay = false;
+                    if (!index) {
+                        signedImageUrlsWithGuidNamesWithUpdatedDisplayValues[
+                            totalLength - 1
+                        ].onDisplay = true;
+                    } else {
+                        signedImageUrlsWithGuidNamesWithUpdatedDisplayValues[index - 1].onDisplay =
+                            true;
+                    }
+                    break;
+                }
+            }
         } else {
-            setCurrentIndex(
-                currentIndex !== signedImageUrlsWithGuidNames.length - 1 ? currentIndex + 1 : 0
-            );
+            for (const [
+                index,
+                imageInfo
+            ] of signedImageUrlsWithGuidNamesWithUpdatedDisplayValues.entries()) {
+                if (imageInfo.onDisplay) {
+                    signedImageUrlsWithGuidNamesWithUpdatedDisplayValues[index].onDisplay = false;
+                    if (index === totalLength - 1) {
+                        signedImageUrlsWithGuidNamesWithUpdatedDisplayValues[0].onDisplay = true;
+                    } else {
+                        signedImageUrlsWithGuidNamesWithUpdatedDisplayValues[index + 1].onDisplay =
+                            true;
+                    }
+                    break;
+                }
+            }
         }
+        setSignedImageUrlsWGuidNamesWDisplayValues([
+            ...signedImageUrlsWithGuidNamesWithUpdatedDisplayValues
+        ]);
     };
 
     const handleDeleteImageButtonClicked = async () => {
-        const imageNameWithGuidToDelete =
-            signedImageUrlsWithGuidNames[currentIndex].imageNameWithGuid;
-        const updatedSignedImageUrlsWithGuidNames = signedImageUrlsWithGuidNames.filter(
-            (signedImageUrlWImageGuidName) =>
-                signedImageUrlWImageGuidName.imageNameWithGuid !== imageNameWithGuidToDelete
+        const currentlyDisplayedImageInfo = getCurrentlyDisplayedImageInfo();
+        const indexToDelete = currentlyDisplayedImageInfo.index;
+        const imageNameWithGuidToDelete = currentlyDisplayedImageInfo.imageNameWithGuid;
+        const updatedSignedImageUrlsWithGuidNames = signedImageUrlsWGuidNamesWDisplayValues.filter(
+            (_, index) => index !== indexToDelete
         );
-        setSignedImageUrlsWithGuidNames(updatedSignedImageUrlsWithGuidNames);
-        setCurrentIndex(currentIndex > 0 ? currentIndex - 1 : 0);
+        if (updatedSignedImageUrlsWithGuidNames.length) {
+            if (indexToDelete === 0) {
+                updatedSignedImageUrlsWithGuidNames[0].onDisplay = true;
+            } else {
+                updatedSignedImageUrlsWithGuidNames[indexToDelete - 1].onDisplay = true;
+            }
+        }
+        setSignedImageUrlsWGuidNamesWDisplayValues(updatedSignedImageUrlsWithGuidNames);
+        setShowDeleteConfirmationButton(false)
         await deleteDocumentImages(collabToken, imageNameWithGuidToDelete, document.documentId);
+    };
+
+    const getCurrentlyDisplayedImageInfo = () => {
+        if (!signedImageUrlsWGuidNamesWDisplayValues.length)
+            return {
+                imageNameWithGuid: '',
+                signedImageUrl: DEFAULT_IMG_URL,
+                index: 0
+            };
+        for (const [index, imageInfo] of signedImageUrlsWGuidNamesWDisplayValues.entries()) {
+            if (imageInfo.onDisplay) {
+                return {
+                    imageNameWithGuid: imageInfo.imageNameWithGuid,
+                    signedImageUrl: imageInfo.signedImageUrl,
+                    index
+                };
+            }
+        }
+        return {
+            imageNameWithGuid: '',
+            signedImageUrl: DEFAULT_IMG_URL,
+            index: 0
+        };
     };
 
     return (
         <div className={`relative h-full w-full flex-col items-center justify-evenly text-center`}>
             <div className="relative box-border flex h-[90%] w-full items-center overflow-hidden rounded-t-[2.5rem] border-x-2 border-t-2 border-gray-100 text-center">
-                {signedImageUrlsWithGuidNames.length && (
+                {signedImageUrlsWGuidNamesWDisplayValues.length && (
                     <button
                         onClick={() => {
                             changeIndex(DIRECTION.LEFT);
@@ -108,7 +173,7 @@ export function ImageCarousel({
                         <FaChevronLeft className="text-[2rem]" />
                     </button>
                 )}
-                {signedImageUrlsWithGuidNames.length && (
+                {signedImageUrlsWGuidNamesWDisplayValues.length && (
                     <button
                         onClick={() => {
                             changeIndex(DIRECTION.RIGHT);
@@ -119,26 +184,18 @@ export function ImageCarousel({
                     </button>
                 )}
                 <img
-                    src={
-                        signedImageUrlsWithGuidNames.length
-                            ? signedImageUrlsWithGuidNames[currentIndex].signedImageUrl
-                            : DEFAULT_IMG_URL
-                    }
+                    src={getCurrentlyDisplayedImageInfo().signedImageUrl}
                     className="absolute z-[2] h-full w-full object-contain"
                     alt=""
                 />
                 <img
-                    src={
-                        signedImageUrlsWithGuidNames.length
-                            ? signedImageUrlsWithGuidNames[currentIndex].signedImageUrl
-                            : DEFAULT_IMG_URL
-                    }
+                    src={getCurrentlyDisplayedImageInfo().signedImageUrl}
                     className="absolute inset-0 z-[1] h-full w-full overflow-hidden rounded-t-[2.5rem] bg-center object-cover opacity-70 blur-sm"
                     alt=""
                 />
             </div>
 
-            <div className="flex h-[10%] w-full items-center justify-between bg-red-200">
+            <div className="flex h-[10%] w-full items-center justify-around bg-red-200">
                 <button
                     onClick={() => {
                         setShowUploadModalInfo({
@@ -149,14 +206,16 @@ export function ImageCarousel({
                     }}
                     className={
                         'mt-0 h-full ' +
-                        (signedImageUrlsWithGuidNames && signedImageUrlsWithGuidNames.length > 0
+                        (signedImageUrlsWGuidNamesWDisplayValues &&
+                        signedImageUrlsWGuidNamesWDisplayValues.length > 0
                             ? 'w-[10rem]'
                             : 'w-full') +
                         ' bg-red-200 p-[0.6rem] text-[0.9rem] transition duration-200 hover:bg-white' +
                         ' flex items-center justify-center text-center'
                     }
                 >
-                    {signedImageUrlsWithGuidNames && signedImageUrlsWithGuidNames.length > 0 ? (
+                    {signedImageUrlsWGuidNamesWDisplayValues &&
+                    signedImageUrlsWGuidNamesWDisplayValues.length > 0 ? (
                         <div className="flex items-center justify-center text-center">
                             Upload images&nbsp;
                             <IoMdCloudUpload className="text-[1rem]" />
@@ -168,19 +227,35 @@ export function ImageCarousel({
                         </div>
                     )}
                 </button>
-                <div className="flex h-full w-[30%] justify-center">
-                    <button
-                        onClick={handleDeleteImageButtonClicked}
-                        className="flex aspect-square h-full items-center justify-center text-center transition duration-200 hover:bg-white"
-                    >
-                        <FaTrashAlt className="text-[1rem]" />
-                    </button>
-                    {signedImageUrlsWithGuidNames && signedImageUrlsWithGuidNames.length > 0 && (
-                        <div className="flex h-full w-[40%] items-center justify-end">
-                            {currentIndex + 1 + '/' + signedImageUrlsWithGuidNames.length}
+
+                {signedImageUrlsWGuidNamesWDisplayValues &&
+                    signedImageUrlsWGuidNamesWDisplayValues.length > 0 && (
+                        <div className="flex h-full w-[30%] justify-center">
+                            <div className="flex flex-col h-full w-[50%] items-center justify-end">
+                                {showDeleteConfirmationButton && (
+                                    <button
+                                        className="rounded bg-red-500 p-2 text-white z-10"
+                                        onClick={handleDeleteImageButtonClicked}
+                                    >
+                                        Delete
+                                    </button>
+                                )}
+
+                                <button
+                                    onClick={() => setShowDeleteConfirmationButton(!showDeleteConfirmationButton)}
+                                    className="flex aspect-square h-full min-h-full items-center justify-center text-center transition duration-200 hover:bg-white"
+                                >
+                                    <FaTrashAlt className="text-[1rem]" />
+                                </button>
+                            </div>
+                            <div className="flex h-full w-[50%] items-center justify-center">
+                                {getCurrentlyDisplayedImageInfo().index +
+                                    1 +
+                                    '/' +
+                                    signedImageUrlsWGuidNamesWDisplayValues.length}
+                            </div>
                         </div>
                     )}
-                </div>
             </div>
         </div>
     );
