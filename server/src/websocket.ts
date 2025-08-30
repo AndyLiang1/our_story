b6943c93497bb8827f0af8ff68b860a1350d10be
AndyLiang1 <andyliang1000@gmail.com>
2025-08-30 15:49:31 -0700

    Updated model to include ydoc. Added update function which works.

--- server/src/websocket.ts
+++ server/src/websocket.ts
@@ -1,15 +1,38 @@
 import { Hocuspocus } from '@hocuspocus/server';
 import { JwtVerifier } from './middleware/JwtVerifier';
+import { Document } from './models/Document'; // your Sequelize model
+import * as Y from 'yjs';
 
 export const initHocuspocusWebsocketServer = async () => {
     const hocuspocusServer = new Hocuspocus({
-        async onAuthenticate(data) {
+        async onAuthenticate({token, context}) {
             try {
-                const decoded = await JwtVerifier.verifyCollabTokenHelper(data.token);
+                const decoded = await JwtVerifier.verifyCollabTokenHelper(token);
                 console.log('Verified JWT for socket connection.');
-                return decoded.userId;
+                context.userId = decoded.userId;
+                return true
             } catch (err) {
                 throw new Error('Not authorized!');
+            }
+        },
+        async onStoreDocument({documentName, document, context}) {
+             try {
+                const state = Y.encodeStateAsUpdate(document);
+                const [doc, created] = await Document.findOrCreate({
+                    where: { documentId: documentName },
+                    defaults: {
+                        documentId: documentName,
+                        ydoc: state,
+                    }
+                });
+
+                if (!created) {
+                    await doc.update({ ydoc: state });
+                }
+
+                console.log(`Document ${documentName} stored successfully.`);
+            } catch (err) {
+                console.error('Error storing document:', err);
             }
         }
     });
