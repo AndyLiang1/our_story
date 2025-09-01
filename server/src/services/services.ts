d81f423aabf4b261c65879b1efcd42c7993d6362
AndyLiang1 <andyliang1000@gmail.com>
2025-09-01 14:03:00 -0700

    Fix uint8 array type on ydoc. Remove tiptap backend service.

--- server/src/services/services.ts
+++ server/src/services/services.ts
@@ -2,20 +2,17 @@ import { DocumentOwnerService } from './DocumentOwnerService';
 import { DocumentService } from './DocumentService';
 import { ImageService } from './ImageService';
 import { PartnerService } from './PartnerService';
-import { TipTapDocumentService } from './TipTapDocumentService';
 import { UserService } from './UserService';
 
 const userService = new UserService();
 const documentService = new DocumentService();
-const tiptapDocumentService = new TipTapDocumentService();
 const imageService = new ImageService();
 const partnerService = new PartnerService();
-const documentOwnerService = new DocumentOwnerService()
+const documentOwnerService = new DocumentOwnerService();
 
 export const services = {
     userService,
     documentService,
-    tiptapDocumentService,
     imageService,
     partnerService,
     documentOwnerService
