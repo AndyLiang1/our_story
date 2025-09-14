46e77a7aa9e58c40b7da880fecf4631a82d78de8
AndyLiang1 <andyliang1000@gmail.com>
2025-09-14 13:37:26 -0700

    Decrease server memory and publicize FE non-sensitive credentials.

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -3,7 +3,7 @@ export const config = {
         import.meta.env.VITE_APP_NODE_ENV == 'production'
             ? 'https://ourstory-api.fly.dev'
             : 'http://localhost:3002',
-    hocuspocusUrl: 
+    hocuspocusUrl:
         import.meta.env.VITE_APP_NODE_ENV == 'production'
             ? 'https://ourstory-api.fly.dev/collaboration'
             : 'ws://localhost:3002/collaboration',
@@ -12,8 +12,8 @@ export const config = {
     },
     cognito: {
         region: 'us-east-2',
-        userPoolId: import.meta.env.VITE_APP_USER_POOL_ID,
-        clientId: import.meta.env.VITE_APP_CLIENT_ID
+        userPoolId: 'us-east-2_ec0wURg0C',
+        clientId: '78ok0p6onnb6tsa2hgsmrcjjc3'
     },
     server: {
         port: import.meta.env.VITE_APP_SERVER_PORT
